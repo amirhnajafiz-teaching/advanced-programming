0d45f0ede4c973c56bd0b263ae8b413bb0268713
AmirH.Najafizadeh <najafizadeh21@gmail.com>
2022-09-17 09:53:02 +0430

    Update setup.sh

--- minikube/macos/setup.sh
+++ minikube/macos/setup.sh
@@ -1,1 +1,9 @@
 #!bin/bash
+
+# to install the latest minikube stable release on x86-64 macOS using binary download
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+sudo install minikube-darwin-amd64 /usr/local/bin/minikube
+
+# starting minikube.
+# from a terminal with administrator access (but not logged in as root), run
+minikube start
