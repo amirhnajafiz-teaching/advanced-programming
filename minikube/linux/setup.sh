2193dbabde9b4df80c921d4e2fdc9ca9a1de97e0
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-17 16:47:47 +0330

    remove: all

--- minikube/linux/setup.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-# to install the latest minikube stable release on x86-64 Linux using binary download
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-
-# start minikube
-# from a terminal with administrator access (but not logged in as root), run
-minikube start
